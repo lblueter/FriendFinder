06a40f8238d223a139443118822d9dbc10db860b
lblueter <logan11023@yahoo.com>
2018-10-31 19:32:49 -0400

    update

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var path = require("path");
+var api = require('./app/routing/apiRoutes')
+var html = require('./app/routing/htmlRoutes')
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
+
+app.use(api)
+app.use(html)
