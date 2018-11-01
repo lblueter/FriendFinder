e600935a77827aef736a9ed1bad821f79c835490
lblueter <logan11023@yahoo.com>
2018-11-01 18:22:17 -0400

    setting up server

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,12 +1,13 @@
 var express = require('express')
+var path = require("path");
 var router = express.Router()
 
 router.get('/', function (req, res) {
-  res.send('home.html')
+  res.sendFile(path.join(__dirname, "./../public/home.html"));
 })
 
 router.get('/survey', function (req, res) {
-  res.send('survey.html')
+  res.send('./../public/survey.html')
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -7,5 +7,12 @@ var app = express();
 
 var PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(api)
 app.use(html)
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
