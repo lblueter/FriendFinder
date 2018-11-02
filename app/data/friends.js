a5f945fb6efdcc51552a785b220b68f9046c5c93
lblueter <logan11023@yahoo.com>
2018-11-01 20:21:13 -0400

    workin on survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,133 @@
 var friends = [
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
   },
-  
+  {
+    "name": "Lauren S",
+    "photo": "https://memeguy.com/photos/images/i-think-i-found-my-favorite-stock-photo-129777.jpg",
+    "scores": [
+      5,
+      5,
+      4,
+      5,
+      5,
+      5,
+      1,
+      4,
+      1,
+      5
+    ]
+  },
+  {
+    "name": "Steve LaPearl",
+    "photo": "https://thumbs.dreamstime.com/z/latino-man-tastes-awful-food-8998438.jpg",
+    "scores": [
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3
+    ]
+  },
+  {
+    "name": "Jerky McJerkface",
+    "photo": "http://archive.4plebs.org/dl/pol/image/1449/09/1449092601539.jpg",
+    "scores": [
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
+    ]
+  },
+  {
+    "name": "Mark P",
+    "photo": "https://static.boredpanda.com/blog/wp-content/uploads/2017/12/funny-weird-wtf-stock-photos-16-5a392518213b6__700.jpg",
+    "scores": [
+      4,
+      5,
+      3,
+      3,
+      5,
+      5,
+      3,
+      2,
+      1,
+      3
+    ]
+  },
+  {
+    "name": "Carl Coffee",
+    "photo": "https://st2.depositphotos.com/2631505/5462/i/950/depositphotos_54626107-stock-photo-happy-businessman-holding-funny-huge.jpg",
+    "scores": [
+      3,
+      5,
+      1,
+      2,
+      5,
+      2,
+      1,
+      1,
+      5,
+      2
+    ]
+  },
+  {
+    "name": "Mark Hamilltime",
+    "photo": "https://thumbs.dreamstime.com/z/funny-crazy-guy-hammer-10882175.jpg",
+    "scores": [
+      5,
+      2,
+      5,
+      2,
+      4,
+      4,
+      2,
+      3,
+      5,
+      5
+    ]
+  },
+  {
+    "name": "Dennis Hopper",
+    "photo": "http://l7.alamy.com/zooms/2416caa4779a463f87a1154ede8288d7/smiling-man-wearing-bunny-ears-and-holding-a-rabbit-cmnnnr.jpg",
+    "scores": [
+      3,
+      4,
+      2,
+      5,
+      5,
+      1,
+      1,
+      3,
+      5,
+      1
+    ]
+  }
+
 ]
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,8 +2,13 @@ var express = require('express')
 var friends = require('./../data/friends')
 var router = express.Router()
 
-router.get('/api/tables', function (req, res) {
+router.get('/api/friends', function (req, res) {
   res.json(friends);
+})
+
+router.post('/api/friends', function (req, res) {
+  newPerson = req.body
+
 })
 
 module.exports = router
