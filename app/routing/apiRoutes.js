d5c56f8ca495259b149f1ec6b8939b2974904712
lblueter <logan11023@yahoo.com>
2018-11-03 10:03:07 -0400

    attempting compatibility

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,12 +2,29 @@ var express = require('express')
 var friends = require('./../data/friends')
 var router = express.Router()
 
+var compare = function () {
+
+}
+function add(a, b) {
+  return a + b;
+}
 router.get('/api/friends', function (req, res) {
   res.json(friends);
 })
 
 router.post('/api/friends', function (req, res) {
-  newPerson = req.body
+  friends.push(req.body)
+  var matchScore = req.body.scores.reduce(add, 0)
+  console.log(matchScore)
+  for (var i = 0; i < friends.length; i++) {
+    var sum = friends[i].scores.reduce(add, 0);
+    console.log(sum)
+
+  }
+
+  
+
+  console.log(sum);
   // Handle incoming survey results
   // Handle compatibility logic 
 
